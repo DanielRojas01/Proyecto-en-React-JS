3ed55faac15644b9d7710ef64c1fe5b6751a294f
Daniel Rojas <daniel.rojas.academico@gmail.com>
2022-09-27 19:35:52 -0300

    funcion alter, funcion contar, funcion aprobados

--- M3U4/js/index.js
+++ M3U4/js/index.js
@@ -1,43 +1,47 @@
+/*EJERCICIO 1*/
+function alter(color){
+    document.querySelector('body').style.background=color;
+}
 
+/*EJERCICIO 2*/ 
+const texto = document.getElementById('texto');
+const contar = document.getElementById('contar');
+texto.addEventListener('keyup', function()
+{contar.innerText = texto.value.length;})
+
+/* EJERCICIO 3*/
 function aprobado(){
-    var alumnos = [{
-        nombre: 'Juan Gomez',
-        nota: 7
-      }, {
-        nombre: 'Pedro Rodriguez',
-        nota: 4
-      }, {
-        nombre: 'Roxana García',
-        nota: 8
-      }, {
-        nombre: 'Luciano Lopez',
-        nota: 5
-      }, {
-        nombre: 'Fernanda Gimenez',
-        nota: 6
-      }, {
-        nombre: 'Florencia Martinez',
-        nota: 10
-      }, {
-        nombre: 'Raul Sanchez',
-        nota: 7
-      }, 
-      {
-        nombre: 'Sandra Figueroa',
-        nota: 8
-      }];
-}
+  var alumnos = [{
+      nombre: 'Juan Gomez',
+      nota: 7
+    }, {
+      nombre: 'Pedro Rodriguez',
+      nota: 4
+    }, {
+      nombre: 'Roxana García',
+      nota: 8
+    }, {
+      nombre: 'Luciano Lopez',
+      nota: 5
+    }, {
+      nombre: 'Fernanda Gimenez',
+      nota: 6
+    }, {
+      nombre: 'Florencia Martinez',
+      nota: 10
+    }, {
+      nombre: 'Raul Sanchez',
+      nota: 7
+    }, 
+    {
+      nombre: 'Sandra Figueroa',
+      nota: 8
+    }];
 
-function alternar(){
-    const alternar = document.getElementsByClassName('alternar')[0];          
-         
-    /**
-     * classList representa la lista de clases asignadas a un elemento
-     * toggle nos permite "prender" o "apagar" una clase en un objeto
-     * add agrega la clase o clases que le pasemos
-     * remove remueve la clase o clases que le pasemos
-     */
-    alternar.addEventListener('click', function(e) {
-        e.currentTarget.classList.toggle('activo');
+    const aprobados = alumnos.filter(function(alumno){
+      return alumno.nota>=7;
     });
-}
+    for(let i=0; i<aprobados.length; i++){
+    document.write(`<li>Alumno ${aprobados[i].nombre}, Nota ${aprobados[i].nota}`)}
+    document.write("<p>Cantidad de aprobados ", aprobados.length);
+  }
