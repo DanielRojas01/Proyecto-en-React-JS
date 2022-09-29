476adbd9d25ff71ac8eb5457ce21a4df03b45cba
Daniel Rojas <daniel.rojas.academico@gmail.com>
2022-09-28 22:50:15 -0300

    header, nav y footer

---
+++ M4U1/transportes/frontend/src/components/layout/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Footer=(props) =>{
+    return(
+      <footer>
+        <p>Curso diplomatura en programacion web con React JS - &copy;2022</p>
+      </footer>);
+}
+export default Footer;
---
+++ M4U1/transportes/frontend/src/components/layout/Nav.js
@@ -0,0 +1,18 @@
+
+import React from 'react-router-dom';
+const Nav =(props) =>{
+    return(    
+      <nav>
+        <div>
+            <ul>
+                <li><link to="/">Home</link></li>
+                <li><link to="/nosotros">Nosotros</link></li>
+                <li><link to="/novedades">Servicios</link></li>
+                <li><link to="/galeria">Galeria</link></li>
+                <li><link to="/novedades">Novedades</link></li>
+                <li><link to="/contacto">Contacto</link></li>
+            </ul>
+        </div>
+      </nav>);
+}
+export default Nav;
