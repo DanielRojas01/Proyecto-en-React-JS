05e10ed03d0cc74becb81d2a919214961f6629e9
Daniel Rojas <daniel.rojas.academico@gmail.com>
2022-10-11 21:08:05 -0300

    nav transition

--- M4U1/Mi portafolio 0.2/frontend/src/components/layout/Nav.js
+++ M4U1/Mi portafolio 0.2/frontend/src/components/layout/Nav.js
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../../styles/layout/Nav.css";
 const Nav = (props) => {
   return (
     <nav>
       <div className="logo">
         <Link to="/">
-          <img className="color" src="img1/logochip.svg" width="60" alt="logo" />
+          <img src="img1/logochip.svg" width="60" alt="logo" />
         </Link>
       </div>
       <div className="navegacion">
-        <Link to="/">Home</Link>
-        <Link to="/servicios">Servicios</Link>
-        <Link to="/clientes">Clientes</Link>
-        <Link to="/conoceme">Conoceme</Link>
-        <Link to="/contacto">Contacto</Link>
+        <NavLink to="/" className={({isActive}) => isActive ? "active" : undefined} >Home</NavLink>
+        <NavLink to="/servicios" className={({isActive}) => isActive ? "active" : undefined} >Servicios</NavLink>
+        <NavLink to="/clientes" className={({isActive}) => isActive ? "active" : undefined} >Clientes</NavLink>
+        <NavLink to="/conoceme" className={({isActive}) => isActive ? "active" : undefined} >Conoceme</NavLink>
+        <NavLink to="/contacto" className={({isActive}) => isActive ? "active" : undefined} >Contacto</NavLink>
       </div>
     </nav>
   );
